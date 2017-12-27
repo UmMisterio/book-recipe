5d0c31d64dfc582d6cf607a1962c050c467a3381
UmMisterio <jvsdf1@gmail.com>
2017-12-27 15:45:44 -0200

    navbar

---
+++ implementacao/index.php
@@ -0,0 +1,46 @@
+<head>
+
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<title>Book Recipe</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+
+<body class="background">
+
+<!--     Head     -->
+	<div class="head"></div>
+		<img src="image/logozx.png" class="logo">
+
+	<a href="#">
+		<img src="image/icon/receitasx.png" class="icon">
+	</a>
+
+	<a href="#">
+		<img src="image/icon/chefs.png" class="iconx">
+	</a>
+
+	<div class="sub-head"></div>
+
+<!--    Buttons   --> 
+	<a href="#"><button class="button">Login</button></a>
+
+	<a href="#"><button class="buttonx">Singup</button></a>
+
+<!--    Sub Head  -->
+	<div class="sub-head-link">
+		<a href="#">Salgados</a>
+
+		<a href="#">Doces</a>
+
+		<a href="#">Comidas Tipicas</a>
+
+		<a href="#">Alimentação Saudável</a>
+
+		<a href="#">Acompanhamentos</a>
+
+		<a href="#">Diversificados</a>
+	</div>
+
+</body>
