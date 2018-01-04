02eec7724ab77f19ddbeb54e0d08c85dd609b577
UmMisterio <jvsdf1@gmail.com>
2018-01-04 15:09:36 -0200

    login e singup

---
+++ implementacao/singup/index.php
@@ -0,0 +1,51 @@
+<head>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<title>Book Recipe</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+
+<body class="background">
+
+	<!-- Header -->
+
+	<a href="#" class="hover"><img src="image/back-login_hover.png" class="control"></a>
+
+	<a href="#" class="not_a_member">Já tem conta?</a>
+
+	<a href="#"><button class="button">Login</button></a>
+
+	<!-- Acess -->
+
+	<img src="image/logo-orange.png" class="logo">
+
+	<hr class="firts">
+
+	<!-- Sing Up -->
+
+	<p class="text_name">Nome</p>
+
+	<input type="text" name="nick" placeholder="zézinho" class="name">
+
+	<div class="container_email">
+		<p class="text_email">Email</p>
+
+		<input type="text" name="Email" placeholder="exemplo@gmail.com" class="email" autofocus>
+	</div>
+
+	<hr>
+
+	<div class="container_password">
+		<p class="text_password">Senha</p>
+
+		<input type="password" name="senha" placeholder="********" class="password">
+	</div>
+
+	<button class="submit">Singup</button>
+
+	<hr>
+
+</body>
