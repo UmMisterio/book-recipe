4ea7a15dbde8e2b2fae3507e13c48ff3c259e2ea
lucasgian <gianextensao@gmail.com>
2018-01-12 13:03:17 -0300

    correções

--- implementacao/core/authentication.js
+++ implementacao/core/authentication.js
@@ -1,119 +1,102 @@
-// Buttons
-var authEmailPassButton = document.getElementById('authEmailPassButton');
-var authFacebookButton = document.getElementById('authFacebookButton');
-var authTwitterButton = document.getElementById('authTwitterButton');
-var authGoogleButton = document.getElementById('authGoogleButton');
-var authGitHubButton = document.getElementById('authGitHubButton');
-var authAnonymouslyButton = document.getElementById('authAnonymouslyButton');
-var createUserButton = document.getElementById('createUserButton');
-var logOutButton = document.getElementById('logOutButton');
-
-// Inputs
-var emailInput = document.getElementById('emailInput');
-var passwordInput = document.getElementById('passwordInput');
-
-// Displays
-var displayName = document.getElementById('displayName');
-
-// Criar novo usuário
-createUserButton.addEventListener('click', function () {
-    firebase
-        .auth()
-        .createUserWithEmailAndPassword(emailInput.value, passwordInput.value)
-        .then(function () {
-            alert('Bem vindo ' + emailInput.value);
-        })
-        .catch(function (error) {
-            console.error(error.code);
-            console.error(error.message);
-            alert('Falha ao cadastrar, verifique o erro no console.')
-        });
-});
-
-// Autenticar com E-mail e Senha
-authEmailPassButton.addEventListener('click', function () {
-    firebase
-        .auth()
-        .signInWithEmailAndPassword(emailInput.value, passwordInput.value)
-        .then(function (result) {
-            console.log(result);
-            displayName.innerText = 'Bem vindo, ' + emailInput.value;
-            alert('Autenticado ' + emailInput.value);
-        })
-        .catch(function (error) {
-            console.error(error.code);
-            console.error(error.message);
-            alert('Falha ao autenticar, verifique o erro no console.')
-        });
-});
-
-// Logout
-logOutButton.addEventListener('click', function () {
-    firebase
-        .auth()
-        .signOut()
-        .then(function () {
-            displayName.innerText = 'Você não está autenticado';
-            alert('Você se deslogou');
-        }, function (error) {
-            console.error(error);
-        });
-});
-
-// Autenticar Anônimo
-authAnonymouslyButton.addEventListener('click', function () {
-    firebase
-        .auth()
-        .signInAnonymously()
-        .then(function (result) {
-            console.log(result);
-            displayName.innerText = 'Bem vindo, desconhecido';
-            alert('Autenticado Anonimamente');
-        })
-        .catch(function (error) {
-            console.error(error.code);
-            console.error(error.message);
-            alert('Falha ao autenticar, verifique o erro no console.')
-        });
-});
-
-// Autenticar com GitHub
-authGitHubButton.addEventListener('click', function () {
-    // Providers
-    var provider = new firebase.auth.GithubAuthProvider();
-    signIn(provider);
-});
-
-// Autenticar com Google
-authGoogleButton.addEventListener('click', function () {
-    // Providers
-    var provider = new firebase.auth.GoogleAuthProvider();
-    signIn(provider);
-});
-
-// Autenticar com Facebook
-authFacebookButton.addEventListener('click', function () {
-    // Providers
-    var provider = new firebase.auth.FacebookAuthProvider();
-    signIn(provider);
-});
-
-// Autenticar com Twitter
-authTwitterButton.addEventListener('click', function () {
-    // Providers
-    var provider = new firebase.auth.TwitterAuthProvider();
-    signIn(provider);
-});
-
-function signIn(provider) {
-    firebase.auth()
-        .signInWithPopup(provider)
-        .then(function (result) {
-            console.log(result);
-            var token = result.credential.accessToken;
-            displayName.innerText = 'Bem vindo, ' + result.user.displayName;
-        }).catch(function (error) {
-            console.log(error);
-            alert('Falha na autenticação');
-        });
+/**
+         * Function called when clicking the Login/Logout button.
+         */
+// [START buttoncallback]
+function toggleSignIn() {
+    if (!firebase.auth().currentUser) {
+        // [START createprovider]
+        var provider = new firebase.auth.GoogleAuthProvider();
+        // [END createprovider]
+        // [START addscopes]
+        provider.addScope('https://www.googleapis.com/auth/plus.login');
+        // [END addscopes]
+        // [START signin]
+        firebase.auth().signInWithRedirect(provider);
+        // [END signin]
+    } else {
+        // [START signout]
+        firebase.auth().signOut();
+        // [END signout]
+    }
+    // [START_EXCLUDE]
+    document.getElementById('quickstart-sign-in').disabled = true;
+    // [END_EXCLUDE]
 }
+// [END buttoncallback]
+/**
+ * initApp handles setting up UI event listeners and registering Firebase auth listeners:
+ *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
+ *    out, and that is where we update the UI.
+ *  - firebase.auth().getRedirectResult(): This promise completes when the user gets back from
+ *    the auth redirect flow. It is where you can get the OAuth access token from the IDP.
+ */
+function initApp() {
+    // Result from Redirect auth flow.
+    // [START getidptoken]
+    firebase.auth().getRedirectResult().then(function (result) {
+        if (result.credential) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
+            // [START_EXCLUDE]
+            document.getElementById('quickstart-oauthtoken').textContent = token;
+        } else {
+            document.getElementById('quickstart-oauthtoken').textContent = 'null';
+            // [END_EXCLUDE]
+        }
+        // The signed-in user info.
+        var user = result.user;
+    }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // [START_EXCLUDE]
+        if (errorCode === 'auth/account-exists-with-different-credential') {
+            alert('You have already signed up with a different auth provider for that email.');
+            // If you are using multiple auth providers on your app you should handle linking
+            // the user's accounts here.
+        } else {
+            console.error(error);
+        }
+        // [END_EXCLUDE]
+    });
+    // [END getidptoken]
+    // Listening for auth state changes.
+    // [START authstatelistener]
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var isAnonymous = user.isAnonymous;
+            var uid = user.uid;
+            var providerData = user.providerData;
+            // [START_EXCLUDE]
+            document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
+            document.getElementById('quickstart-sign-in').textContent = 'Sign out';
+            document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
+            // [END_EXCLUDE]
+        } else {
+            // User is signed out.
+            // [START_EXCLUDE]
+            document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
+            document.getElementById('quickstart-sign-in').textContent = 'Sign in with Google';
+            document.getElementById('quickstart-account-details').textContent = 'null';
+            document.getElementById('quickstart-oauthtoken').textContent = 'null';
+            // [END_EXCLUDE]
+        }
+        // [START_EXCLUDE]
+        document.getElementById('quickstart-sign-in').disabled = false;
+        // [END_EXCLUDE]
+    });
+    // [END authstatelistener]
+    document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
+}
+window.onload = function () {
+    initApp();
+};
